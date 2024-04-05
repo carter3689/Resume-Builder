5969c8d941943a9c289122d51f572936c2671458
joel.carter <joel.carter@waterfurnace.com>
2024-04-04 23:56:11 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <main class="container">
+      <div id="resume" class="d-flex">
+        <div class="left-col">
+          <h4 class="section-headline">
+            About Me
+          </h4>
+          <h4 class="section-headline">
+            Contact
+          </h4>
+          <h4 class="section-headline">
+            Skills
+          </h4>
+          <h4 class="section-headline">
+            certification
+          </h4>
+        </div>
+        <div class="right-col">
+          <h4 class="section-headline">
+            Experience
+            </h4>
+            <h4 class="section-headline">
+              Education
+            </h4>
+        </div>
+      </div>
+    </main>
+</template>
+
+<style scoped="">
+</style>
